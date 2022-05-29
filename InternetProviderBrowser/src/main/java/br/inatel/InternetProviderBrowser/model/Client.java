0a7d0ce147b59330d350ee0556882b43788765f1
Gustavo Rocha Flores <31926341+Dekkard@users.noreply.github.com>
2022-05-29 17:33:27 -0300

    update Client.java

--- InternetProviderBrowser/src/main/java/br/inatel/InternetProviderBrowser/model/Client.java
+++ InternetProviderBrowser/src/main/java/br/inatel/InternetProviderBrowser/model/Client.java
@@ -1,5 +1,6 @@
 package br.inatel.InternetProviderBrowser.model;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -29,6 +30,8 @@ public class Client {
 	@NotNull
 	private Long cpf;
 	private LocalDate birthDate;
+	private BigDecimal lat;
+	private BigDecimal lng;
 	@ManyToMany
 	private List<Plan> listPlan = new ArrayList<>();
 
@@ -39,34 +42,42 @@ public class Client {
 //		this.id = c.getId();
 		this.name = c.getName();
 		this.cpf = c.getCpf();
-		this.birthDate = getBirthDate();
+		this.birthDate = c.getBirthDate();
+		this.lat = c.getLat();
+		this.lng = c.getLng();
 	}
 
 	public Client(@Length(max = 255) @NotNull String name, @Digits(integer = 11, fraction = 0) @NotNull Long cpf,
-			LocalDate birthDate) {
+			LocalDate birthDate, BigDecimal lat, BigDecimal lng) {
 		super();
 		this.name = name;
 		this.cpf = cpf;
 		this.birthDate = birthDate;
+		this.lat = lat;
+		this.lng = lng;
 	}
 
 	public Client(Long id, @Length(max = 255) @NotNull String name, @Digits(integer = 11, fraction = 0) @NotNull Long cpf,
-			LocalDate birthDate) {
+			LocalDate birthDate, BigDecimal lat, BigDecimal lng) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.cpf = cpf;
 		this.birthDate = birthDate;
+		this.lat = lat;
+		this.lng = lng;
 	}
 
 	public Client(Long id, @Length(max = 255) @NotNull String name, @Digits(integer = 11, fraction = 0) @NotNull Long cpf,
-			LocalDate birthDate, List<Plan> listPlan) {
+			LocalDate birthDate, BigDecimal lat, BigDecimal lng, List<Plan> listPlan) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.cpf = cpf;
 		this.birthDate = birthDate;
 		this.listPlan = listPlan;
+		this.lat = lat;
+		this.lng = lng;
 	}
 
 	public Long getId() {
@@ -80,7 +91,12 @@ public class Client {
 	public Long getCpf() {
 		return cpf;
 	}
-
+public BigDecimal getLat()  {
+	return lat;
+	}
+	public BigDecimal getLng()  {
+	return lng;
+	}
 	public List<Plan> getPlan() {
 		return listPlan;
 	}
@@ -100,14 +116,20 @@ public class Client {
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
 	}
+	public void setLat(BigDecimal lat){
+		this.lat = lat;
+		}
+		public void setLng(Bigdecimal lng){
+			this.lng = lng;
+			}
 
-	public void setListService(List<Plan> listService) {
-		this.listPlan = listService;
+	public void setListPlan(List<Plan> listPlan) {
+		this.listPlan = listPlan;
 	}
 
 	public static ClientDTO modeltoDTO(Client c) {
 		ClientDTO clientDTO = new ClientDTO(c.getId(), c.getName(), c.getCpf(),
-				c.getBirthDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+				c.getBirthDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")), Client.getLat().getPlainString(), Client.getLng().getPlainString());
 
 		return clientDTO;
 	}
